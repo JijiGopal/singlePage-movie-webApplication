85fd11b4a05db27649265e0824677854d172cab9
Jijina KK <jijigopal2000@gmail.com>
2023-10-20 15:54:46 +0530

    redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+
+// import actions from homeSlice
+import {getApiConfiguration,getGenres} from './store/homeSlice.js';
+
+import {fetchApiData} from './utils/api.js';
 
 function App() {
-  
+  const dispatch = useDispatch()
+  const {url} = useSelector((state) => state.home );
+  console.log(url)
+
+  useEffect(()=>{
+    apiTest();
+  },[])
+
+  const apiTest = ()=>{
+    fetchApiData('/movie/popular')
+    .then((res) =>{
+      console.log("responsee",res)
+      dispatch(getApiConfiguration(res))
+    })
+  }
+
   return (
-    <>
-    </>
+    <div className='App'>
+      {url?.total_pages}
+    </div>
   )
 }
 
