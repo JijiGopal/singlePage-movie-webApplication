d9a3a0d302aee28f539ae27244b7c738e4c8f5d1
Jijina KK <jijigopal2000@gmail.com>
2023-10-20 20:29:28 +0530

    components and pages are created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import { useSelector, useDispatch } from 'react-redux';
+import {fetchApiData} from './utils/api.js';
 // import actions from homeSlice
 import {getApiConfiguration,getGenres} from './store/homeSlice.js';
 
-import {fetchApiData} from './utils/api.js';
+import Header from './components/header/Header.jsx';
+import Footer from './components/footer/Footer.jsx';
+import Home from './pages/home/Home.jsx';
+import SearchResult from './pages/searchresult/searchResult.jsx';
+import Details from './pages/details/Details.jsx';
+import Explore from './pages/explore/Explore.jsx';
+import PageNotFound from './pages/404/pageNotFound.jsx';
+
+
+
 
 function App() {
   const dispatch = useDispatch()
@@ -24,9 +35,17 @@ function App() {
   }
 
   return (
-    <div className='App'>
-      {url?.total_pages}
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element = {<Home/>} />
+        <Route path="/:mediaType/:id" element = {<Details/>} />
+        <Route path="/search/:query" element = {<SearchResult/>} />
+        <Route path="/explore/:mediaType" element = {<Explore/>} />
+        <Route path="*" element = {<PageNotFound/>} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   )
 }
 
