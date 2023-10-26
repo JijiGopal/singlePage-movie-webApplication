f21a73285010137220d92810ddb7b04b34b39383
Jijina KK <jijigopal2000@gmail.com>
2023-10-26 15:33:19 +0530

    modified

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,10 @@ import HeroBanner from './heroBanner/HeroBanner';
 
 const Home = () => {
   return (
-    <div className='homePage'><HeroBanner/></div>
+    <div className='homePage'>
+      <HeroBanner/>
+      <div style={{height:1000}}></div>
+    </div>
   )
 }
 
