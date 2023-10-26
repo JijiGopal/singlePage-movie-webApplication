0e6c2db1b0c17e5f41ced3857ef09878b2db73e0
Jijina KK <jijigopal2000@gmail.com>
2023-10-26 22:27:37 +0530

    changes to add genres in carousel part

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { useSelector, useDispatch } from 'react-redux';
-import {fetchApiData} from './utils/api.js';
+import { fetchApiData } from './utils/api.js';
 // import actions from homeSlice
-import {getApiConfiguration,getGenres} from './store/homeSlice.js';
+import { getApiConfiguration, getGenres } from './store/homeSlice.js';
 
 import Header from './components/header/Header.jsx';
 import Footer from './components/footer/Footer.jsx';
@@ -19,38 +19,56 @@ import PageNotFound from './pages/404/pageNotFound.jsx';
 
 function App() {
   const dispatch = useDispatch()
-  const {url} = useSelector((state) => state.home );
+  const { url } = useSelector((state) => state.home);
   console.log(url)
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchApiConfig();
-  },[])
+    genresCall();
+  }, [])
 
-  const fetchApiConfig = ()=>{
+  const fetchApiConfig = () => {
     fetchApiData('/configuration')
-    .then((res) =>{
-      // console.log("responsee",res)
-      const url =  {
-        backdrop: res.images.secure_base_url + "original",
-        poster:res.images.secure_base_url + "original",
-        profile:res.images.secure_base_url + "original",
+      .then((res) => {
+        // console.log("responsee",res)
+        const url = {
+          backdrop: res.images.secure_base_url + "original",
+          poster: res.images.secure_base_url + "original",
+          profile: res.images.secure_base_url + "original",
 
-      }
-      dispatch(getApiConfiguration(url))
+        }
+        dispatch(getApiConfiguration(url))
+      })
+  }
+
+  const genresCall = async () => {
+    let promises = []
+    let endPoints = ["tv", "movie"]
+    let allGenres = {}
+
+    endPoints.forEach((url) => {
+      promises.push(fetchApiData(`/genre/${url}/list`))
+    });
+
+    const data = await Promise.all(promises)
+    // console.log("hj",data)
+    data.map(({genres}) => {
+      return genres.map((item) => (allGenres[item.id] = item))
     })
+    dispatch(getGenres(allGenres));
   }
 
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element = {<Home/>} />
-        <Route path="/:mediaType/:id" element = {<Details/>} />
-        <Route path="/search/:query" element = {<SearchResult/>} />
-        <Route path="/explore/:mediaType" element = {<Explore/>} />
-        <Route path="*" element = {<PageNotFound/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   )
 }
