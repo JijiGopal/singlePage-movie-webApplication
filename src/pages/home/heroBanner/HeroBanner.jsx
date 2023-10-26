e3fa3c2cecaad8fe4fc4752a6b969369cb0cbf6b
Jijina KK <jijigopal2000@gmail.com>
2023-10-26 12:19:59 +0530

    modified and styles added

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,27 +1,30 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import "./style.scss";
 
 // import usefetch
 import useFetch from '../../../hooks/useFetch.jsx';
 
+import Img from '../../../components/lazyLoadImage/Img.jsx';
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper.jsx';
+
 const HeroBanner = () => {
   const [background, setBackground] = useState("");
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
-  const {url} = useSelector((state) => state.home)
-  const {data, loading} = useFetch("/movie/upcoming")
+  const { url } = useSelector((state) => state.home)
+  const { data, loading } = useFetch("/movie/upcoming")
 
   useEffect(() => {
-    
-    const bg = url.backdrop + data?.results[Math.floor(Math.random() *20)]?.backdrop_path
+
+    const bg = url.backdrop + data?.results[Math.floor(Math.random() * 20)]?.backdrop_path
     setBackground(bg);
   }, [data])
 
-  const searchQueryHandler = (event) =>{
-    console.log("keyyy",event.key)
-    if(event.key === "Enter" && query.length > 0){
+  const searchQueryHandler = (event) => {
+    console.log("keyyy", event.key)
+    if (event.key === "Enter" && query.length > 0) {
       navigate(`/search/${query}`);
     }
   }
@@ -30,29 +33,34 @@ const HeroBanner = () => {
   return (
     <div className='heroBanner'>
 
-      <div className="backdrop-img">
-    
+      {!loading && <div className="backdrop-img">
+        <Img src={background} />
+      </div>}
+
+      <div className="opacity-layer">
+        
       </div>
 
-      <div className="wrapper">
-        <div className="heroBannerContent">
-          <span className="title">Welcom.</span>
-          <span className="subTitle">
-            Millions of movies,
-            TV shows and people to discover.
-            Explore Now.
-          </span>
+      <ContentWrapper > 
+          <div className="heroBannerContent">
+            <span className="title">Welcom.</span>
+            <span className="subTitle">
+              Millions of movies,
+              TV shows and people to discover.
+              Explore Now.
+            </span>
 
-          <div className="searchInput">
-            <input 
-            type="text" 
-            placeholder='Search for a movie or TV show ...'
-            onChange={(e) => setQuery(e.target.value)}
-            onKeyDown={searchQueryHandler}/>
-            <button>Search</button>
+            <div className="searchInput">
+              <input
+                type="text"
+                placeholder='Search for a movie or TV show ...'
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyDown={searchQueryHandler} />
+              <button>Search</button>
+            </div>
           </div>
-        </div>
-      </div>
+      </ContentWrapper>
+
     </div>
   );
 }
