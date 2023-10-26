55e11837031b83ea6b012d6b854fcd6c8c252e86
Jijina KK <jijigopal2000@gmail.com>
2023-10-26 22:40:58 +0530

    Carousel left and right arrows activated

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -21,7 +21,14 @@ const Carousel = ({data, loading}) => {
     const navigate = useNavigate();
 
     const navigation = (dir) => {
+        const container = carouselContainer.current;
+        const scrollAmount = dir === "left" ? container.scrollLeft - 
+        (container.offsetWidth + 20) : container.scrollLeft + (container.offsetWidth + 20);
 
+    container.scrollTo({
+        left : scrollAmount,
+        behavior : "smooth",
+    })
     }
 
     const skItem = () => {
@@ -29,10 +36,10 @@ const Carousel = ({data, loading}) => {
             <div className="skeletonItem">
                 <div className="posterBlock skeleton">
                     <div className="textBlock">
-                        <div className="title">
+                        <div className="title skeleton">
 
                         </div>
-                        <div className="date">
+                        <div className="date skeleton">
 
                         </div>
                     </div>
@@ -51,13 +58,15 @@ const Carousel = ({data, loading}) => {
             className="carouselRightNav arrow"
             onClick={() => navigation("right")}/>
             {!loading ? (
-                <div className="carouselItems">
+                <div className="carouselItems"
+                ref={carouselContainer}>
                     {data?.map((item) => {
                         const posterUrl = item.poster_path ? 
                         url.poster + item.poster_path : 
                         PosterFallback
                         return (
-                            <div key={item.id} className="carouselItem">
+                            <div key={item.id} className="carouselItem" 
+                            onClick={() => navigate(`/${item.media_type}/${item.id}`)}>
                                 <div className="posterBlock">
                                     <Img src = {posterUrl} />
                                     <CircleRating rating={item.vote_average.toFixed(1)}
