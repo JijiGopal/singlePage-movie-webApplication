5ecdf3453a9d27c7d5648ab3b4e82f91d25eb666
Jijina KK <jijigopal2000@gmail.com>
2023-10-28 13:36:55 +0530

    modified

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -77,7 +77,7 @@ const Header = () => {
     <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
       <ContentWrapper>
 
-        <div className="logo">
+        <div className="logo" onClick={() => navigate("/")}>
           <img src={logo} />
         </div>
 
